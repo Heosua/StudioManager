6a58c1add07cf572a42bc3d26601c0165469b197
Ho Minh Sang <38745048+Heosua@users.noreply.github.com>
2019-02-28 18:05:37 +0700

    asp.net app

---
+++ StudioManager/StudioManager/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudioManager.Models;
+
+namespace StudioManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<StudioOrder> StudioOrderList = new List<StudioOrder>
+        {
+            new StudioOrder
+            {
+                Customer   = "Grab" ,
+                Address = "Lĩnh Nam Hoàng Mai",
+                Phonenumber = "0348340941",
+                Location = "Ba Vì Hà Nôi"
+            },
+            new StudioOrder
+            {
+                Customer   = "Uber" ,
+                Address = "Đội Cấn Ba đình",
+                Phonenumber = "0348346574",
+                Location = "Bãi đá sông hồng Hà Nôi"
+            },
+            new StudioOrder
+            {
+                Customer   = "Bee" ,
+                Address = "Thụy khê Tây hồ",
+                Phonenumber = "0348340941",
+                Location = "Tuyệt tình cốc "
+            },
+            new StudioOrder
+            {
+                Customer   = "Emddi" ,
+                Address = "Ngọc Khánh Kim Mã",
+                Phonenumber = "03483234567",
+                Location = "Lotte Bulding"
+            }
+        };
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(IFormCollection collection)
+        {
+            StudioOrder stu = new StudioOrder(); // tạo mới một đơn hàng rồi set các thuộc tính từ form html
+
+
+            stu.Customer = collection["Customer"];
+            stu.Phonenumber = collection["Phonenumber"];
+            stu.Location = collection["Location"];
+            stu.Address = collection["Address"];
+            StudioOrderList.Add(stu);
+            if (CreateConfirm(stu) == true)
+            {
+                return View(stu);
+            }
+            else
+            {
+                return View(new StudioOrder { Customer = "false", Phonenumber = "false", Location = "false", Address = "false" });
+            }
+        }
+        public bool CreateConfirm(StudioOrder stu)
+        {
+            if (StudioOrderList.Contains(stu))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public ActionResult ViewList()
+        {
+            return View(StudioOrderList);
+        }
+
+       [HttpGet]
+        public ActionResult Delete(StudioOrder stu)
+        {
+            //StudioOrder stu = new StudioOrder();
+              
+            //stu.Customer = collection["Customer"];
+            //stu.Phonenumber = collection["Phonenumber"];
+            //stu.Address = collection["Address"];
+            //stu.Location = collection["Location"];
+            if (StudioOrderList.Contains(stu))
+            {
+                StudioOrderList.Remove(stu);
+               
+            }
+            return View(stu);
+        }
+
+    }
+}
---
+++ StudioManager/StudioManager/Models/StudioOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudioManager.Models
+{
+    public class StudioOrder
+    {
+        public string Customer { get; set; }
+        public string Address { get; set; }
+        public string Phonenumber { get; set;}
+        public string Location { get; set; }
+
+
+    }
+}
